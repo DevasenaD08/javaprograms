ceffbc2fe227b4c49adecef113ac4251a79e60fb
Devasena D <154534945+DevasenaD08@users.noreply.github.com>
2025-07-14 16:33:53 +0530

    Add files via upload
    upper and lowercase prg

---
+++ MethodsJava.java
@@ -0,0 +1,10 @@
+package Sample_2;
+
+public class MethodsJava {
+ public static void main(String[] args) {
+	String s1="DevaseNA";
+	System.out.println(s1.toUpperCase());
+	String s2=s1.toLowerCase();
+	System.out.println(s2);
+}
+}
