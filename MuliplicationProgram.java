5e326eaf1966ea536edd759ba1c9a046b5428d52
Devasena D <154534945+DevasenaD08@users.noreply.github.com>
2025-07-15 15:35:39 +0530

    Add files via upload

---
+++ MuliplicationProgram.java
@@ -0,0 +1,14 @@
+package Sample_2;
+
+public class MuliplicationProgram {
+	public static void main(String[] args) {
+		int table=13;
+		//1*13=13
+		//2*13=26
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i+ "*" + table+ " = " +i*table);
+		}
+	}
+
+}
