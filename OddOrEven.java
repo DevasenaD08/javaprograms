d070a3e4dcdee8beb6398add84be9125fc53c507
Devasena D <154534945+DevasenaD08@users.noreply.github.com>
2025-07-15 13:23:42 +0530

    Add files via upload
    week1 prgs

---
+++ OddOrEven.java
@@ -0,0 +1,16 @@
+package Sample_2;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		
+	int n=10;
+	if(n%2==0)
+	{
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+
+}
+}
